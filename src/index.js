5ea146805cd4680f96386df78130bad6d1b3e3e4
Onesinus SPT <onesinus231@gmail.com>
2020-04-13 23:01:49 +0700

    Layouting

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'bulma/css/bulma.css'
+import './styles/main.css';
+
+const main = () => {
+    const getWeather = () => {
+        console.log("this is getting weather list");
+    }
+
+    getWeather();
+}
+
+main();
