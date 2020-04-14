8a19c4df854bf07cc03f369214d521d5716e66a5
Onesinus SPT <onesinus231@gmail.com>
2020-04-15 01:15:08 +0700

    Data already depends on menu clicked, for now Jakarta, Bali, DIY, Sumatera Utara, Lampung, Aceh

--- src/components/menu-left.js
+++ src/components/menu-left.js
@@ -1,29 +1,31 @@
+import $ from "jquery";
+
+/* Required Tools */
+const convert = require('xml-js');
+/* Required Tools */
 class MenuLeft extends HTMLElement{
     constructor() {
         super();
-        this._city = "Jakarta";
-    }
-
-    set city(city) {
-        this._city = city;
-        this.render();
+        this.city = 'DKIJakarta';
     }
 
     render() {
-        console.log(this._city);
+        console.log(this.city);
         this.innerHTML = `
             <img src="../icon.png" alt="Logo">
             <aside class="menu">
                 <ul class="menu-list">
-                    <li><a class=${this._city === 'Jakarta' ? 'is-active':''}>Jakarta</a></li>
-                    <li><a class=${this._city === 'Bali' ? 'is-active':''}>Bali</a></li>
-                    <li><a class=${this._city === 'DI Yogyakarta' ? 'is-active':''}>DI Yogyakarta</a></li>
-                    <li><a class=${this._city === 'Sumatera Utara' ? 'is-active':''}>Sumatera Utara</a></li>
-                    <li><a class=${this._city === 'Lampung' ? 'is-active':''}>Lampung</a></li>
-                    <li><a class=${this._city === 'Aceh' ? 'is-active':''}>Aceh</a></li>
+                    <li><a onclick='document.getElementsByTagName("menu-left")[0].setAttribute("city", "DKIJakarta")' class=${this.city === 'DKIJakarta' ? 'is-active':''}>Jakarta</a></li>
+                    <li><a onclick='document.getElementsByTagName("menu-left")[0].setAttribute("city", "Bali")' class=${this.city === 'Bali' ? 'is-active':''}>Bali</a></li>
+                    <li><a onclick='document.getElementsByTagName("menu-left")[0].setAttribute("city", "DIYogyakarta")' class=${this.city === 'DIYogyakarta' ? 'is-active':''}>DI Yogyakarta</a></li>
+                    <li><a onclick='document.getElementsByTagName("menu-left")[0].setAttribute("city", "SumateraUtara")' class=${this.city === 'SumateraUtara' ? 'is-active':''}>Sumatera Utara</a></li>
+                    <li><a onclick='document.getElementsByTagName("menu-left")[0].setAttribute("city", "Lampung")' class=${this.city === 'Lampung' ? 'is-active':''}>Lampung</a></li>
+                    <li><a onclick='document.getElementsByTagName("menu-left")[0].setAttribute("city", "Aceh")' class=${this.city === 'Aceh' ? 'is-active':''}>Aceh</a></li>
                 </ul>
             </aside>        
         `;
+
+        this.getWeather(`https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-${this.city}.xml`);
     }
 
     connectedCallback() {
@@ -33,6 +35,53 @@ class MenuLeft extends HTMLElement{
     disconnectedCallback() {
         this.innerHTML = ``;
     }
+
+    attributeChangedCallback(city, oldValue, newValue) {
+        console.log(`woi`);
+        if (oldValue !== newValue) {            
+            this[city] = newValue;
+            this.render();
+        }
+    }
+
+    static get observedAttributes() {
+        return ["city"]
+    }
+
+    // Fetching Data
+    async getWeather(url) {
+        try {
+                const weather  = await fetch(url)
+                                    .then(response => response.text())
+                                    .then(str => JSON.parse(convert.xml2json(str, { compact: true, spaces: 4 })))
+                        
+            const tableContent = $("._list-weather tbody");
+            tableContent.empty();
+    
+            
+            const areas = weather.data.forecast.area || [];
+            const { year, month, day, hour, minute, second } = weather.data.forecast.issue;
+            $(".display-datetime").text(`Last Updated Data: ${day._text}-${month._text}-${year._text} ${hour._text}:${minute._text}:${second._text}`);
+            areas.map((area, idx) => {
+                tableContent.append(`
+                    <tr>
+                        <th>${idx+1}</th>
+                        <td>${area.name[0]._text}</td>
+                        <td>${area._attributes.longitude}</td>
+                        <td>${area._attributes.latitude}</td>
+                        <td>${area.parameter[3].timerange[0].value._text}% - ${area.parameter[1].timerange[0].value._text}%</td>
+                        <td>${area.parameter[4].timerange[0].value[0]._text}°C - ${area.parameter[2].timerange[0].value[0]._text}°C</td>
+                        <td>${area.parameter[3].timerange[1].value._text}% - ${area.parameter[1].timerange[1].value._text}%</td>
+                        <td>${area.parameter[4].timerange[1].value[0]._text}°C - ${area.parameter[2].timerange[1].value[0]._text}°C</td>
+                        <td>${area.parameter[3].timerange[2].value._text}% - ${area.parameter[1].timerange[2].value._text}%</td>
+                        <td>${area.parameter[4].timerange[2].value[0]._text}°C - ${area.parameter[2].timerange[2].value[0]._text}°C</td>
+                    </tr>
+                `);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }        
 }
 
 customElements.define("menu-left", MenuLeft);
--- src/index.js
+++ src/index.js
@@ -3,56 +3,8 @@ import 'bulma/css/bulma.css'
 import './styles/main.css';
 /* End Load Css */
 
-/* Load JS */
-import $ from "jquery";
-/* End Load JS */
-
 /* Import Components  */
 import './components/menu-left';
 import './components/table-one';
 import './components/footer-one';
 /* End Import Components */
-
-/* Required Tools */
-const convert = require('xml-js');
-/* Required Tools */
-
-const main = () => {
-    const getWeather = async(url) => {
-        try {
-                const weather  = await fetch(url)
-                                    .then(response => response.text())
-                                    .then(str => JSON.parse(convert.xml2json(str, { compact: true, spaces: 4 })))
-                        
-            const tableContent = $("._list-weather tbody");
-            tableContent.empty();
-
-            
-            const areas = weather.data.forecast.area || [];
-            const { year, month, day, hour, minute, second } = weather.data.forecast.issue;
-            $(".display-datetime").text(`Last Updated Data: ${day._text}-${month._text}-${year._text} ${hour._text}:${minute._text}:${second._text}`);
-            areas.map((area, idx) => {
-                tableContent.append(`
-                    <tr>
-                        <th>${idx+1}</th>
-                        <td>${area.name[0]._text}</td>
-                        <td>${area._attributes.longitude}</td>
-                        <td>${area._attributes.latitude}</td>
-                        <td>${area.parameter[3].timerange[0].value._text}% - ${area.parameter[1].timerange[0].value._text}%</td>
-                        <td>${area.parameter[4].timerange[0].value[0]._text}°C - ${area.parameter[2].timerange[0].value[0]._text}°C</td>
-                        <td>${area.parameter[3].timerange[1].value._text}% - ${area.parameter[1].timerange[1].value._text}%</td>
-                        <td>${area.parameter[4].timerange[1].value[0]._text}°C - ${area.parameter[2].timerange[1].value[0]._text}°C</td>
-                        <td>${area.parameter[3].timerange[2].value._text}% - ${area.parameter[1].timerange[2].value._text}%</td>
-                        <td>${area.parameter[4].timerange[2].value[0]._text}°C - ${area.parameter[2].timerange[2].value[0]._text}°C</td>
-                    </tr>
-                `);
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    getWeather('https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-DKIJakarta.xml');
-}
-
-main();
