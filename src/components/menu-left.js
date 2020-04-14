92b2e8e11a7c81ba3e1eb8eaddb0b87c19c26dc2
Onesinus SPT <onesinus231@gmail.com>
2020-04-14 11:13:45 +0700

    Ubah menu jadi beberapa provinsi diindonesia

--- src/components/menu-left.js
+++ src/components/menu-left.js
@@ -4,12 +4,12 @@ class MenuLeft extends HTMLElement{
             <img src="../icon.png" alt="Logo">
             <aside class="menu">
                 <ul class="menu-list">
-                <li><a class="is-active">Jakarta Timur</a></li>
-                <li><a>Jakarta Barat</a></li>
-                <li><a>Jakarta Selatan</a></li>
-                <li><a>Jakarta Utara</a></li>
-                <li><a>Jakarta Pusat</a></li>
-                <li><a>Kepulauan Seribu</a></li>
+                <li><a class="is-active">Jakarta</a></li>
+                <li><a>Bali</a></li>
+                <li><a>DI Yogyakarta</a></li>
+                <li><a>Sumatera Utara</a></li>
+                <li><a>Lampung</a></li>
+                <li><a>Aceh</a></li>
                 </ul>
             </aside>        
         `;
