19350263ac2271e9a342079744e591da378af2b4
Onesinus SPT <onesinus231@gmail.com>
2020-04-14 13:10:59 +0700

    Sudah mendapatkan data dan parsing dari xml ke json dan sudah append ke table untuk wilayah jakarta

--- src/components/table-one.js
+++ src/components/table-one.js
@@ -2,15 +2,14 @@ class TableOne extends HTMLElement{
     render() {
         this.innerHTML = `
                 <div class="table-container _list-weather">
-                    <table class="table">
+                    <h1 class="display-datetime"></h1>
+                    <table class="table is-fullwidth">
                         <thead>
-                        <tr>
+                        <tr class="is-selected">
                             <th>#</th>
-                            <th>Area Code</th>
+                            <th>Area</th>
                             <th><abbr title="Longitude">Longitude</abbr></th>
                             <th><abbr title="Latitude">Latitude</abbr></th>
-                            <th><abbr title="Date">Date</abbr></th>
-                            <th><abbr title="Time">Time</abbr></th>
                             <th><abbr title="Percentage">%</abbr></th>
                             <th><abbr title="Celcius">C</abbr></th>
                             <th><abbr title="Fahrenheit">F</abbr></th>
@@ -18,18 +17,7 @@ class TableOne extends HTMLElement{
                         </tr>
                         </thead>
                         <tbody>
-                        <tr class="is-selected">
-                            <th>1</th>
-                            <td>501192</td>
-                            <td>106.731319</td>
-                            <td>-6.203019</td>
-                            <td>13-04-2020</td>
-                            <td>05:30:10</td>
-                            <td>90</td>
-                            <td></td>
-                            <td></td>
-                            <td>Aman Terkendali</td>
-                        </tr>
+
                         </tbody>
                     </table>
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+/* Load Css */
 import 'bulma/css/bulma.css'
 import './styles/main.css';
+/* End Load Css */
+
+/* Load JS */
+import $ from "jquery";
+/* End Load JS */
 
 /* Import Components  */
 import './components/menu-left';
@@ -7,12 +13,45 @@ import './components/table-one';
 import './components/footer-one';
 /* End Import Components */
 
+/* Required Tools */
+const convert = require('xml-js');
+/* Required Tools */
+
 const main = () => {
-    const getWeather = () => {
-        console.log("this is getting weather list");
+    const getWeather = async(url) => {
+        try {
+                const weather  = await fetch(url)
+                                    .then(response => response.text())
+                                    .then(str => JSON.parse(convert.xml2json(str, { compact: true, spaces: 4 })))
+                        
+            const tableContent = $("._list-weather tbody");
+            tableContent.empty();
+
+            
+            const areas = weather.data.forecast.area || [];
+            console.log(weather.data.forecast.issue);
+            const { year, month, day, hour, minute, second } = weather.data.forecast.issue;
+            $(".display-datetime").text(`Date / Time: ${day._text}-${month._text}-${year._text} ${hour._text}:${minute._text}:${second._text}`);
+            areas.map((area, idx) => {
+                tableContent.append(`
+                    <tr>
+                        <th>${idx+1}</th>
+                        <td>${area.name[0]._text}</td>
+                        <td>${area._attributes.longitude}</td>
+                        <td>${area._attributes.latitude}</td>
+                        <td>90</td>
+                        <td></td>
+                        <td></td>
+                        <td>Aman Terkendali</td>
+                    </tr>
+                `);
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    getWeather();
+    getWeather('https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-DKIJakarta.xml');
 }
 
 main();
